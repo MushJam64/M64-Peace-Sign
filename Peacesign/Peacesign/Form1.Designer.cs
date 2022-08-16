43892cd94a64e4bec3be899f1ecfd34b474030d1
Bakejess200 <100545041+Bakejess200@users.noreply.github.com>
2022-08-16 16:45:14 -0400

    Made everything a drop down menu instead of buttons
    More compact more fun

---
+++ Peacesign/Peacesign/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Peacesign__alpha_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboboxHands = new System.Windows.Forms.ComboBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboboxEye = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboboxHands);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(326, 70);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hand Controls";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // comboboxHands
+            // 
+            this.comboboxHands.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboboxHands.FormattingEnabled = true;
+            this.comboboxHands.Items.AddRange(new object[] {
+            "Fists",
+            "Open",
+            "Right Open",
+            "Peace Sign",
+            "Cap In Hand",
+            "Wingcap In Hand"});
+            this.comboboxHands.Location = new System.Drawing.Point(8, 28);
+            this.comboboxHands.MaxDropDownItems = 6;
+            this.comboboxHands.Name = "comboboxHands";
+            this.comboboxHands.Size = new System.Drawing.Size(312, 23);
+            this.comboboxHands.TabIndex = 10;
+            this.comboboxHands.Text = "Fists";
+            this.comboboxHands.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button9.Location = new System.Drawing.Point(12, 272);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 25);
+            this.button9.TabIndex = 6;
+            this.button9.Text = "Refresh";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboboxEye);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(12, 88);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(326, 70);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Eye Controls";
+            // 
+            // comboboxEye
+            // 
+            this.comboboxEye.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboboxEye.FormattingEnabled = true;
+            this.comboboxEye.Items.AddRange(new object[] {
+            "Blinking",
+            "Open",
+            "Half",
+            "Closed",
+            "Up",
+            "Down",
+            "Left",
+            "Right"});
+            this.comboboxEye.Location = new System.Drawing.Point(8, 28);
+            this.comboboxEye.Name = "comboboxEye";
+            this.comboboxEye.Size = new System.Drawing.Size(312, 23);
+            this.comboboxEye.TabIndex = 10;
+            this.comboboxEye.Text = "Blinking";
+            this.comboboxEye.SelectedIndexChanged += new System.EventHandler(this.comboboxEye_SelectedIndexChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboBox2);
+            this.groupBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox3.Location = new System.Drawing.Point(12, 164);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(326, 70);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "X3 Controls (Exmo3)";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Soon"});
+            this.comboBox2.Location = new System.Drawing.Point(6, 28);
+            this.comboBox2.MaxDropDownItems = 6;
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(312, 23);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.Text = "Soon";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(185, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 60);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "M64 Peace Sign\r\nMade with V64Core\r\nMade By Violet64 with help \r\nfrom Llennpie\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 316);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "M64 Peace Sign";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private GroupBox groupBox1;
+        private Button button9;
+        private ComboBox comboboxHands;
+        private GroupBox groupBox2;
+        private ComboBox comboboxEye;
+        private GroupBox groupBox3;
+        private ComboBox comboBox2;
+        private Label label1;
+    }
+}
---
+++ Peacesign/Peacesign/Form1.cs
@@ -0,0 +1,233 @@
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibV64Core;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Peacesign__alpha_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Process[] emulatorProcesses = Memory.GetEmulatorProcesses("Project64");
+            Memory.HookEmulatorProcess(emulatorProcesses[0]);
+            Memory.FindBaseAddress();
+
+            if (Core.CameraFrozen)
+                Debug.WriteLine("Camera was already frozen!");
+
+            Core.FixCameraZoomOut();
+            Core.FixResetBodyState();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Core.SetHandState(Types.HandState.PEACE);
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Core.SetEyeState(Types.EyeState.HALF);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Core.SetEyeState(Types.EyeState.CLOSED);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Core.SetEyeState(Types.EyeState.UP);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Core.SetEyeState(Types.EyeState.DOWN);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Core.SetEyeState(Types.EyeState.LEFT);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Core.SetEyeState(Types.EyeState.RIGHT);
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            Core.FixResetBodyState();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog(); // Shows Form2
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboboxHands.Text)
+            {
+                case "Fists":
+                    // fist
+                    Core.SetHandState(Types.HandState.FISTS);
+                    break;
+                case "Open":
+                    // open
+                    Core.SetHandState(Types.HandState.OPEN);
+                    break;
+                case "Right Open":
+                    // Right hand open only
+                    Core.SetHandState(Types.HandState.RIGHT_OPEN);
+                    break;
+                case "Peace Sign":
+                    // Peace Sign
+                    Core.SetHandState(Types.HandState.PEACE);
+                    break;
+                case "Cap In Hand":
+                    // Cap in hand
+                    Core.SetHandState(Types.HandState.WITH_CAP);
+                    break;
+                case "Wingcap In Hand":
+                    // Wingcap in hand
+                    Core.SetHandState(Types.HandState.WITH_WING_CAP);
+                    break;
+            }
+        }
+        
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboboxEye_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboboxEye.Text)
+            {
+                case "Blinking":
+                    // Blinking
+                    Core.SetEyeState(Types.EyeState.BLINKING);
+                    break;
+                case "Open":
+                    // open
+                    Core.SetEyeState(Types.EyeState.OPEN);
+                    break;
+                case "Half":
+                    // half
+                    Core.SetEyeState(Types.EyeState.HALF);
+                    break;
+                case "Closed":
+                    // closed
+                    Core.SetEyeState(Types.EyeState.CLOSED);
+                    break;
+                case "Up":
+                    // unused up
+                    Core.SetEyeState(Types.EyeState.UP);
+                    break;
+                case "Down":
+                    // unused down
+                    Core.SetEyeState(Types.EyeState.DOWN);
+                    break;
+                case "Right":
+                    // unused right
+                    Core.SetEyeState(Types.EyeState.RIGHT);
+                    break;
+                case "Left":
+                    // unused left
+                    Core.SetEyeState(Types.EyeState.LEFT);
+                    break;
+                case "Dead":
+                    // dead eyes
+                    Core.SetEyeState(Types.EyeState.DEAD);
+                    break;
+            }
+        }
+    }
+}
+
